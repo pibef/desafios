5b802514fe1f97cc7f393530dd78e63f939f3aca
pipet <125613728+pibef@users.noreply.github.com>
2024-04-19 20:03:11 -0300

    Add files via upload

---
+++ javas/tuto.js
@@ -0,0 +1,16 @@
+/*nombre = "dalto";
+frase = nombre;
+
+document.write(frase);
+// cuando escribas codigo html usa las`` y si queres usar " o ` en unsa frase no uses el mismo que usaste para cerrar 
+*/
+
+/*
+let resultado = valor && valor2;
+// si las 2 son true, true
+let resultado2 = valor || valor2;
+// si una es verdadera, es true
+let resultado3 = !valor;
+// si es verdadero, false
+*/
+
